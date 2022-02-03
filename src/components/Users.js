d5d6196f3d67f2b92c9ab0626b1e84e939492429
andrey12v <andrey12v@gmail.com>
2022-02-02 23:35:16 -0600

    Add UseFinder functional component
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599702#overview

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,19 +3,8 @@ import { Component } from 'react';
 import User from './User';
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class Users extends Component {
-  //with the class components state is always object and it will always have reserved word state
-  //with class components you have to group all variables in one object
-  //with functional components state can be anything - boolean, number or object 
-  //const [showUsers, setShowUsers] = useState(true) - this is boolean state
   constructor() {
-    //we use super because we are extending our class from another class 
     super();
     this.state = {
       showUsers: true,
@@ -24,23 +13,21 @@ class Users extends Component {
   }
 
   toggleUsersHandler() {
-    // this.state.showUsers = false; // This is not how we do it with class components
-    // with class components we have to use the function setState to set value of states
-    // react merges the state you set with the one that was initially declared in class components 
-    // here we passed the function to the state to update showUsers variable
-    this.setState( (curState) => {return { showUsers: !curState.showUsers };} );
+    // this.state.showUsers = false; // NOT!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
   }
 
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
     );
-    
-    //this.toggleUsersHandler.bind(this) - we use this to reference function in the class
+
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
