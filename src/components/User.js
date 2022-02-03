c503aa356d3d51f5b0ed3ff70cf08d3ee571f041
andrey12v <andrey12v@gmail.com>
2022-02-02 22:42:05 -0600

    Convert from functional to class based component "User"
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599696?start=45#overview

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+//for class based component we need to extend from Component in order to use props
+//with this approach we don't pass porps but we use them inside of the class
+//we can use class based components with functional components 
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  } 
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
